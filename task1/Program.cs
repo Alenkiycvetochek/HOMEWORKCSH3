b0c422ceba50626cda83f24cba4625e20f4d2885
Alina Alinushka <alinaalinushka@MacBook-Pro-Alina.local>
2022-07-15 23:40:08 +0300

    created task1

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void SecondDig (int number)
+
+{
+
+int twodigital = number/10;
+int result = twodigital %10;
+
+Console.WriteLine ("SecondDig" + result);
+}
+
+Console.WriteLine("input number:");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+SecondDig(number1);
