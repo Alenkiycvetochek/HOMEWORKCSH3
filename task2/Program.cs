39a275031e925f8715396ae1a2145f64c55cdc53
Alina Alinushka <alinaalinushka@MacBook-Pro-Alina.local>
2022-07-16 00:10:39 +0300

    created task2

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void FindDigit (string numb)
+{
+    if (numb.Length >= 3)
+    {
+       char thirdChar = numb [2];
+       Console.WriteLine("Third digital " + thirdChar);
+    }
+    
+    else
+    {
+        if (numb.Length == 2 || numb.Length ==1)
+        {
+            Console.WriteLine("This numb has not third digital");
+        }
+    }
+    
+}
+
+string numb;
+Console.Write("Input your numb: ");
+numb = Console.ReadLine();
+FindDigit (numb);
