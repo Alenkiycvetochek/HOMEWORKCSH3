77062bdb68afb21be0f1854e1349250b4a5368bf
Alina Alinushka <alinaalinushka@MacBook-Pro-Alina.local>
2022-07-16 00:59:33 +0300

    created task3

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void FindDayofWeek (int numb)
+
+{
+if (numb <= 5)
+{
+    Console.WriteLine ("this day is weekday");
+}
+
+
+if (numb ==6 || numb == 7)
+{
+Console.WriteLine ("this day is weekend");
+
+}
+
+}
+
+Console.WriteLine ("input numb");
+
+int numb1 = Convert.ToInt32 (Console.ReadLine());
+FindDayofWeek ( numb1);
